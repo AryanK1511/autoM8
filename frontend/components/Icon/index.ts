8601adb61fbf58a27b6fb2ce96e2c8e3613f4669
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-11 16:15:04 -0400

    Add some more icons

--- frontend/components/Icon/index.ts
+++ frontend/components/Icon/index.ts
@@ -4,3 +4,7 @@ export { DocsIcon } from './DocsIcon';
 export { CheckIcon } from './CheckIcon';
 export { SearchIcon } from './SearchIcon';
 export { UserIcon } from './UserIcon';
+export { ExploreIcon } from './ExploreIcon';
+export { LogoutIcon } from './LogoutIcon';
+export { ProfileIcon } from './ProfileIcon';
+export { SaveIcon } from './SaveIcon';
