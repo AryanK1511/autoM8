5e4d1f2fd24dabeae40e31c70d1bbce6e11a7cc7
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-10 20:45:19 -0400

    Add TS interfaces

---
+++ frontend/types/navbar/checkIcon.ts
@@ -0,0 +1,8 @@
+import { SVGProps } from 'react';
+
+// ========== INTERFACE FOR CHECK ICON ==========
+export interface CheckIconProps extends SVGProps<SVGSVGElement> {
+  size?: number;
+  height?: number;
+  width?: number;
+}
