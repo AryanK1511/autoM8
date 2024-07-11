7a7df9ea0b7c7cad4faeeca4991268c1f1ef1014
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-11 16:17:02 -0400

    Export all components

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -2,4 +2,15 @@
 export { HomeBanner } from './HomeBanner/HomeBanner';
 export { Navbar } from './Navbar/Navbar';
 export { SearchBar } from './SearchBar/SearchBar';
-export { GitHubIcon, DocsIcon, CheckIcon, SearchIcon, UserIcon } from './Icon';
+export {
+  GitHubIcon,
+  DocsIcon,
+  CheckIcon,
+  SearchIcon,
+  UserIcon,
+  ExploreIcon,
+  LogoutIcon,
+  ProfileIcon,
+  SaveIcon,
+} from './Icon';
+export { ProfileDropdown } from './ProfileDropdown/ProfileDropdown';
